4dd0b26816cac2c4a199810742669448d7cb6847
Joodddd <joodddd@Jooui-MacBook-Pro-2.local>
2013-12-10 02:37:46 +0900

    update
    update

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
-var bt1,bt2;
-var pg1,pg2;
+var bt1,bt2,bt3;
+var pg1,pg2,pg3;
 
 var init = function() {
   bt1 = document.querySelector('#Bt1');
   bt2 = document.querySelector('#Bt2');
+  bt3 = document.querySelector('#Bt3');
   pg1 = document.querySelector('#Pg1');
   pg2 = document.querySelector('#Pg2');
+  pg3 = document.querySelector('#Pg3');
   
   bt1.onclick = function(e) {
     Pg1.style.opacity = '100'; 
-    Pg2.style.opacity = '0'; 
+    Pg2.style.opacity = '0';
+    Pg3.style.opacity = '0'; 
   };
   bt2.onclick = function(e) {
     Pg1.style.opacity = '0'; 
     Pg2.style.opacity = '100'; 
+    Pg3.style.opacity = '0';
+  };
+  bt3.onclick = function(e) {
+    Pg1.style.opacity = '0'; 
+    Pg2.style.opacity = '0'; 
+    Pg3.style.opacity = '100';
   };
   
 };
